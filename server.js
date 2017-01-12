f672457fce19a0bd872fac6f2745a12bcd26edcc
maurogestoso <maurogestoso@gmail.com>
2017-01-12 10:53:32 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('It works!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}...`)
+});
