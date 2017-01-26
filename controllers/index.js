db435737a7850a44c18df4b56ab5048233df24bf
Tague Ferguson <tagueferguson@live.co.uk>
2017-01-26 12:46:35 +0000

    [FEAT-21] Add api/s3policy route (#27)

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ module.exports = {
   Events: require('./events.controller'),
   Authentication: require('./authentication.controller'),
   Tags: require('./tags.controller'),
-  Resources: require('./resources.controller')
+  Resources: require('./resources.controller'),
+  getPolicy: require('./s3.controller')
 };
--- routes/api.router.js
+++ routes/api.router.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const {events, resources, tags} = require('.');
 const router = express.Router();
+const {getPolicy} = require('../controllers');
+
 
 router.use('/events', events);
 router.use('/resources', resources);
 router.use('/tags', tags);
+
+router.get('/s3policy', (req, res) => {
+  res.json(getPolicy());
+})
 
 // ERROR HANDLING
 router.use(function (error, req, res, next) { // eslint-disable-line no-unused-vars
